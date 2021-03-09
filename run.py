f82127021e4f490e5a8e9d5ae05ca97dbc3b8d50
takumiiimo <[assistant@gmail.com]>
2021-03-10 00:23:50 +0900

    run.pyの編集

--- run.py
+++ run.py
@@ -12,14 +12,5 @@ def hello_person(user_id):
     return "Hello " + user_id
 
 
-@app.toute('/test', methods=['GET', 'POST'])
-def post_test():
-    if request.method == 'GET':
-        pass
-    else:
-        pass
-    return '0'
-
-
 if __name__ == '__main__':
     app.run()
